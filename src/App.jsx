0cf47e9a7b4b83fdf85cad09b64089317ee9698f
BrunoBazIvan <131315692+BrunoBazIvan@users.noreply.github.com>
2024-05-28 20:52:59 -0300

    inicio routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import  { NavBarr } from './components/NavBar'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    <NavBarr />
-    <ItemListContainer
-    />
-    </>
+    <BrowserRouter>
+      <NavBarr />
+      <Routes>
+        <Route  path="/" element= {<ItemListContainer/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
